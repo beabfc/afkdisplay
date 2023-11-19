000eaf94b6ed50e8c7ec84ea8bd5d71f49a01299
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2023-11-19 13:34:24 -0500

    added more potential mod conflict warnings
    antilogout
    sessility
    playtime-tracker

--- src/main/java/io/github/beabfc/afkdisplay/util/AfkDisplayConflicts.java
+++ src/main/java/io/github/beabfc/afkdisplay/util/AfkDisplayConflicts.java
@@ -4,20 +4,60 @@ import java.util.Collection;
 import java.util.Iterator;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 
 public class AfkDisplayConflicts {
     public static boolean checkMods() {
+        String modTarget;
         String modVer;
+        String modName;
+        ModMetadata modData;
         boolean modCheck = true;
 
-        // Check for SvrUtil --> /afk command
-        if (FabricLoader.getInstance().isModLoaded("svrutil")) {
-            modVer = FabricLoader.getInstance().getModContainer("svrutil").get().getMetadata().getVersion()
-                    .getFriendlyString();
-            AfkDisplayLogger.warn("SrvUtil-" + modVer
+        // Check for svrutil --> /afk command primarily, the rest is ok
+        modTarget = "svrutil";
+        if (FabricLoader.getInstance().isModLoaded(modTarget)) {
+            modData = FabricLoader.getInstance().getModContainer(modTarget).get().getMetadata();
+            modVer = modData.getVersion().getFriendlyString();
+            modName = modData.getName();
+            AfkDisplayLogger.warn(modName + "-" + modVer
                     + " has been found, please verify that the /afk command is disabled under config/svrutil/commands.json.");
             modCheck = false;
         }
+
+        // Check for antilogout --> /afk command, and changes timeout behavior's
+        // (Remove)
+        modTarget = "antilogout";
+        if (FabricLoader.getInstance().isModLoaded(modTarget)) {
+            modData = FabricLoader.getInstance().getModContainer(modTarget).get().getMetadata();
+            modVer = modData.getVersion().getFriendlyString();
+            modName = modData.getName();
+            AfkDisplayLogger.warn(modName + "-" + modVer
+                    + " has been found, please remove this mod to avoid AFK timeout confusion.");
+            modCheck = false;
+        }
+
+        // Check for sessility --> changes timeout behavior's (Remove)
+        modTarget = "sessility";
+        if (FabricLoader.getInstance().isModLoaded(modTarget)) {
+            modData = FabricLoader.getInstance().getModContainer(modTarget).get().getMetadata();
+            modVer = modData.getVersion().getFriendlyString();
+            modName = modData.getName();
+            AfkDisplayLogger.warn(modName + "-" + modVer
+                    + " has been found, please remove this mod to avoid AFK timeout confusion.");
+            modCheck = false;
+        }
+        // Check for playtime-tracker --> changes timeout behavior's (Remove)
+        modTarget = "playtime-tracker";
+        if (FabricLoader.getInstance().isModLoaded(modTarget)) {
+            modData = FabricLoader.getInstance().getModContainer(modTarget).get().getMetadata();
+            modVer = modData.getVersion().getFriendlyString();
+            modName = modData.getName();
+            AfkDisplayLogger.warn(modName + "-" + modVer
+                    + " has been found, please remove this mod to avoid AFK timeout/player list confusion.");
+            modCheck = false;
+        }
+
         return modCheck;
     }
 
